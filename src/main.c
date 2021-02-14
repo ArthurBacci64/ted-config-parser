90f6bab2f45af36b208049e32ee5d57b0cde0f88
Arthur Bacci <arthurbacci@protonmail.com>
2021-02-14 19:32:13 -0300

    Basic parsing

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(int argc, char **argv) {
     if (argc < 2) {
@@ -15,11 +16,61 @@ int main(int argc, char **argv) {
         return 1;
     }
     
+    char section[50];
+    section[0] = '\0';
     char c;
-    for (unsigned int i = 0; (c = fgetc(fp)) != EOF; i++) {
-        printf("%c", c);
+    for (int i = 0; (c = fgetc(fp)) != EOF; i++) {
+        if (c == '\r') continue; // Ignore \r
+        if (c == '\n') {
+            i = -1;
+            continue;
+        }
+        if (i == 0 && c == '[') {
+            unsigned int j = 0;
+            while (j < 49) {
+                c = fgetc(fp);
+                if (c == '\\')
+                    c = fgetc(fp);
+                else if (c == ']')
+                    break;
+                section[j] = c;
+                j++;
+            }
+            section[j] = '\0';
+        } else {
+            char key[50];
+            char value[1000];
+            unsigned int j;
+            for (j = 0;; j++) {
+                key[j] = c;
+                c = fgetc(fp);
+                if (c == '\\') {
+                    c = fgetc(fp);
+                    continue;
+                }
+                if (c == '\r')
+                    c = fgetc(fp);
+                if (c == '\n' || c == '=')
+                    break;
+            }
+            key[++j] = '\0';
+            c = fgetc(fp);
+            for (j = 0;; j++) {
+                value[j] = c;
+                c = fgetc(fp);
+                if (c == '\\') {
+                    c = fgetc(fp);
+                    continue;
+                }
+                if (c == '\r')
+                    c = fgetc(fp);
+                if (c == '\n')
+                    break;
+            }
+            value[++j] = '\0';
+            printf("%s :: %s -> %s\n", section, key, value);
+        }
     }
-    printf("\n");
     
     fclose(fp);
     return 0;
